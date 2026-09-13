63b6e2132b1be05885fa184a69c090d091a0a569
Daniel <dgutensohn03@yahoo.com>
2026-09-13 11:56:19 -0600

    Add editable customer domain model

--- InsuranceDashboard.Shared/Models/Customer.cs
+++ InsuranceDashboard.Shared/Models/Customer.cs
@@ -1,9 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace InsuranceDashboard.Shared.Models;
 
-public record Customer(
-    int Id,
-    string Name,
-    string Email,
-    string State,
-    int PolicyCount,
-    decimal TotalPremium);
+public class Customer
+{
+    public Customer() { }
+
+    public Customer(int id, string name, string email, string state, int policyCount, decimal totalPremium)
+    {
+        Id = id; Name = name; Email = email; State = state; PolicyCount = policyCount; TotalPremium = totalPremium;
+    }
+
+    public int Id { get; set; }
+
+    [Required, StringLength(80, MinimumLength = 2)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required, EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [Required, StringLength(2, MinimumLength = 2)]
+    public string State { get; set; } = "CO";
+
+    public int PolicyCount { get; set; }
+    public decimal TotalPremium { get; set; }
+}
