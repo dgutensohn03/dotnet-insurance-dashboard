173731d446325372a54a63ef2ffcd803cb079a22
Daniel <dgutensohn03@yahoo.com>
2026-09-14 04:14:25 -0600

    Add claim and policy update operations

--- InsuranceDashboard.Client/Services/IInsuranceDataService.cs
+++ InsuranceDashboard.Client/Services/IInsuranceDataService.cs
@@ -9,8 +9,10 @@ public interface IInsuranceDataService
     Task<IReadOnlyList<Customer>> GetCustomersAsync();
     Task<DashboardSummary> GetSummaryAsync();
     Task<Claim> AddClaimAsync(Claim claim);
+    Task<Claim> UpdateClaimAsync(Claim claim);
     Task<Claim> ArchiveClaimAsync(int id);
     Task<Claim> RestoreClaimAsync(int id);
+    Task<Policy> UpdatePolicyAsync(Policy policy);
     Task<Customer> AddCustomerAsync(Customer customer);
     Task<Customer> UpdateCustomerAsync(Customer customer);
     Task<Customer> ArchiveCustomerAsync(int id);
