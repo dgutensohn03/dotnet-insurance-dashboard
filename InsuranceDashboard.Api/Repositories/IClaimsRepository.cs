5bce667279928d69a0421b91b0a0e0773cf1d3a3
Daniel <dgutensohn03@yahoo.com>
2026-09-14 04:15:02 -0600

    Add claim update repository operation

--- InsuranceDashboard.Api/Repositories/IClaimsRepository.cs
+++ InsuranceDashboard.Api/Repositories/IClaimsRepository.cs
@@ -7,6 +7,7 @@ public interface IClaimsRepository
     Task<IReadOnlyList<Claim>> GetAllAsync();
     Task<Claim?> GetByIdAsync(int id);
     Task<Claim> AddAsync(Claim claim);
+    Task<Claim?> UpdateAsync(int id, Claim claim);
     Task<Claim?> ArchiveAsync(int id);
     Task<Claim?> RestoreAsync(int id);
 }
